a901520b60a6be99e451c4da9891417b089194f3
Michael Yearsley <Michaelyearsley@outlook.com>
2020-02-01 18:32:36 -0800

    Add project files.

---
+++ SchoolTracker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            
+            var studentName = new List<String>();
+            var grade = new List<int>();
+
+            var adding = true;
+
+            while (adding)
+            {
+                Console.Write("What is the student name: ");
+                studentName.Add(Console.ReadLine());
+                Console.Write("What is the student grade: ");
+                grade.Add(int.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Add another? y/n");
+                if (Console.ReadLine() != "y")
+                    adding = false;
+
+            }
+
+            for (int i = 0; i < studentName.Count; i++)
+            
+            {
+                Console.WriteLine("name:" + studentName[i] + ", "+ "grade:" + grade[i]);
+            }
+        }
+         
+    }   
+}
